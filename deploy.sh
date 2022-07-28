bddfc27139c926f3e649d3f16fb8e503646ef40c
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-07-28 23:20:11 +0800

    Automatic ci test

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Dystopia-user181/The-Second-Alterhistorian.git main:gh-pages
+
+cd -
