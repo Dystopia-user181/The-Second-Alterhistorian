46295ca4f6936fd018055ce40624106c4596ebe5
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-08-06 23:08:34 +0800

    Fix autoseller bug

--- src/js/database/machines/auto-seller.js
+++ src/js/database/machines/auto-seller.js
@@ -13,10 +13,10 @@ GameDatabase.machines.autoSeller = {
 	}],
 	outputs: [],
 	customLoop(diff) {
-		if (!this.inputItem(0)) return;
-		const currency = this.inputItem(0).resource;
 		Machine.addInputHistory(this);
 		Machine.addOutputHistory(this);
+		if (!this.inputItem(0)) return;
+		const currency = this.inputItem(0).resource;
 		const amt = Stack.removeFromStack(this.inputs[0].data, 20 * diff);
 		player.money += Currencies[currency].value * amt;
 		Pipe.tickPipes(this, diff);
