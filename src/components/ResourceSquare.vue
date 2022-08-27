6bc46790862cef9aac5795b369f17d9b2476044a
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-08-27 22:51:02 +0800

    Add a square to distinguish between resourced (#15)

---
+++ src/components/ResourceSquare.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { Currencies } from "@/js/database/currencies.ts";
+
+const { type, size } = defineProps({
+	type: {
+		type: String,
+		required: true
+	},
+	size: {
+		type: String,
+		default: "1em"
+	}
+});
+</script>
+
+<template>
+	<div
+		v-if="Currencies[type]"
+		class="c-resource-square"
+		:style="{
+			'background-color': Currencies[type].colour,
+			'width': size,
+			'height': size
+		}"
+	/>
+</template>
+
+<style scoped>
+.c-resource-square {
+	display: inline-block;
+	vertical-align: middle;
+}
+</style>
