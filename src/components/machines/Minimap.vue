5d0f153ac50b48f90ef4acdc0a581789ce92c03c
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-12-24 12:41:28 +0800

    Improve minimap styles based on machine UI

--- src/components/machines/Minimap.vue
+++ src/components/machines/Minimap.vue
@@ -5,7 +5,7 @@ import { Towns } from "@/js/towns";
 
 import { onMount } from "@/components/mixins";
 
-const WIDTH = 180, HEIGHT = 180;
+const WIDTH = 180, HEIGHT = 160;
 const canvas = $ref(null);
 let ctx;
 function render() {
@@ -17,15 +17,27 @@ function render() {
 		-Towns("current").playerData.display.offset.x,
 		-Towns("current").playerData.display.offset.y
 	);
+	ctx.lineWidth = 25;
 	for (const machine of Machines[player.currentlyIn]) {
 		if (machine.hasWholeBuyableUpgrades) {
-			ctx.fillStyle = "#ffd700";
+			ctx.strokeStyle = "#ffd700";
+			ctx.fillStyle = "#ffd70088";
 		} else if (machine.hasPartialBuyableUpgrades) {
-			ctx.fillStyle = "#00cc00";
+			ctx.strokeStyle = "#00dd00";
+			ctx.fillStyle = "#00cc0088";
 		} else {
-			ctx.fillStyle = "#999999";
+			ctx.strokeStyle = "#999999";
+			ctx.fillStyle = "#99999988";
 		}
-		ctx.fillRect(machine.data.x - 100, machine.data.y - 100, 200, 200);
+		ctx.beginPath();
+		ctx.roundRect(
+			machine.data.x,
+			machine.data.y,
+			90 * (machine.inputs.length + machine.outputs.length),
+			270, [25]
+		);
+		ctx.fill();
+		ctx.stroke();
 	}
 }
 
@@ -40,12 +52,17 @@ onMount({
 </script>
 
 <template>
-	<canvas
-		ref="canvas"
-		class="c-minimap"
-		width="180"
-		height="180"
-	/>
+	<div class="c-minimap">
+		<div class="c-minimap__header">
+			Minimap
+		</div>
+		<canvas
+			ref="canvas"
+			class="c-minimap__canvas"
+			:width="WIDTH"
+			:height="HEIGHT"
+		/>
+	</div>
 </template>
 
 <style scoped>
@@ -56,5 +73,18 @@ onMount({
 	background-color: #242424bb;
 	z-index: 3;
 	border-bottom-left-radius: 10px;
+}
+
+.c-minimap__header {
+	font-size: 17px;
+	font-weight: bold;
+	text-align: center;
+	margin: 5px 0 0;
+}
+
+.c-minimap__canvas {
+	background-color: #121212bb;
+	border-radius: 6px;
+	margin: 6px;
 }
 </style>
